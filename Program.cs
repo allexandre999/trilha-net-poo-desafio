66c2e54fb184940f4dc1c68c67d744dbbd28a76e
Joao Alexandre <alexandre_efeta@hotmail.com>
2022-11-17 08:51:40 -0300

    Desafio do Celular em POO Realizadogit status!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+System.Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "654321", modelo: "Modelo 0", imei: "001970010100000", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+System.Console.WriteLine("\n");
+
+System.Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero: "6542", modelo: "Modelo 1", imei: "001970110103050", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("TikTok");
